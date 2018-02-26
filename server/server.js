56eb59317ed39ae23ad2b4c09c898fd9a0859e19
power_840 <liuyu19921017@gmail.com>
2018-02-26 16:55:18 +0800

    add babel-plugin-import

--- server/server.js
+++ server/server.js
@@ -1,5 +1,40 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// 连接mongoDB
+const DB_URL = 'mongodb://localhost:27017/boss_app';
+
+
+mongoose.connect(DB_URL);
+
+mongoose.connection.on('connected', function () {
+  console.log('mongo connect success')
+})
+// user表
+const User = mongoose.model('user', new mongoose.Schema({
+  user: {type: String, require: true},
+  age: {type: Number, require: true}
+}))
+// 新增数据
+// User.create({
+//   user: 'bbb',
+//   age: 19
+// }, function (err, doc) {
+//   if (!err) {
+//     console.log('success', doc)
+//   } else {
+//     console.log('err', err)
+//   }
+// })
+// 删除
+User.remove({user: 'bbb'}, function (err, doc) {
+  console.log(doc)
+})
+// 更新
+// User.update({user: 'aaa'}, {$set: {age: 12}}, function (err, doc) {
+//   console.log(doc)
+// })
+// 新建app
 const app = express();
 
 app.get('/', function (req, res) {
@@ -7,7 +42,18 @@ app.get('/', function (req, res) {
 });
 
 app.get('/data', function (req, res) {
-  res.json('{name: 123}')
+  // User.findOne({age: 12}, function (err, doc) {
+  //   console.log(doc)
+  // })
+  User.find({}, function (err, doc) {
+    if (!err) {
+      console.log('success', doc)
+      res.json(doc)
+    } else {
+      console.log('err', err)
+    }
+  })
+  // res.json('{name: 123}')
 })
 
 app.listen('9093', function () {
--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Button, List} from 'antd-mobile'
+// import 'antd-mobile/dist/antd-mobile.css'
 
-class App extends Component {
+class App extends React.Component{
   render() {
+    const boss = '李云荣';
+    const firstLeader = '张大喵'
+    const secondLeader = '张二喵'
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>独立团, 团长{boss}</h2>
+        <FirstTeam leader={firstLeader}></FirstTeam>
+        <SecondTeam leader={secondLeader}></SecondTeam>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+function SecondTeam(props) {
+  return <h2>骑兵连, 老大{props.leader}</h2>
+}
+
+class FirstTeam extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      solders: ['aaa', 'bbb', 'ccc']
+    }
+    // this.addSolder = this.addSolder.bind(this)
+  }
+  componentWillMount() {
+    console.log('组件将要加载')
+  }
+  componentDidMount() {
+    console.log('组件已经加载')
+  }
+  addSolder = () => {
+    console.log('addSolder')
+    this.setState({
+      solders: [...this.state.solders, 'new solder ' + Math.random()]
+    })
+  }
+  render() {
+    console.log('组件正在加载')
+    return (
+      <div>
+        <h2>一营营长, {this.props.leader}</h2>
+        <Button type="primary" onClick={this.addSolder}>button</Button>
+        <List renderHeader={ () => 'solder list'}>
+          {this.state.solders.map((v, i)=> {
+            return <List.Item key={i}>{v}</List.Item>
+          })}
+        </List>
+      </div>
+    )
+  }
+}
+
+export default App
